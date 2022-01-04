c0c1427d312ef0e397ed9d6ee47073bbc3211576
Ana Duta <ana.duta@trencadis.ro>
2022-01-04 14:07:38 +0200

    login-component

--- web/mps/src/app/app-routing.module.ts
+++ web/mps/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {LoginComponent} from "./components/login/login.component";
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
