e5aad01469d84659dd6dcfe189225c4155b6f002
Ana Duta <ana.duta@trencadis.ro>
2022-01-04 19:25:36 +0200

    navigate to login page solved

--- web/mps/src/app/app-routing.module.ts
+++ web/mps/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
+import {HomeComponent} from "./components/home/home.component";
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
   }
 ];
 
